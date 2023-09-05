af63b35a8df180b4d8a7dbcf0d04a55cecce09ae
Edward Chew <ewchew@ucdavis.edu>
2023-09-04 20:20:35 -0700

    Add interview ace, update README

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,32 @@
-import Link from 'next/link';
-import Head from 'next/head';
+import Link from "next/link";
+import Head from "next/head";
 
-import Layout from '../../components/layouts/MainLayout'
-import PrimaryProject from '@/../components/PrimaryProject'
-import SecondaryProject from '@/../components/SecondaryProject'
+import Layout from "../../components/layouts/MainLayout";
+import PrimaryProject from "@/../components/PrimaryProject";
+import SecondaryProject from "@/../components/SecondaryProject";
 
-import Arrow from '@/../public/images/Arrow.svg'
-import ArrowWhite from '@/../public/images/ArrowWhite.svg'
-import Edward from '@/../public/images/Home/Edward.jpg'
+import Arrow from "@/../public/images/Arrow.svg";
+import ArrowWhite from "@/../public/images/ArrowWhite.svg";
+import Edward from "@/../public/images/Home/Edward.jpg";
 // Primary Projects
-import CodeLabCommunity from '@/../public/images/Home/PrimaryProjects/CodeLabCommunity.png'
-import KommaMeetings from '@/../public/images/Home/PrimaryProjects/KommaMeetings.png'
-import HandsTogether from '@/../public/images/Home/PrimaryProjects/HandsTogether.png'
-import Translation from '@/../public/images/Home/PrimaryProjects/Translation.png'
-import CodeLabBrand from '@/../public/images/Home/PrimaryProjects/CodeLabBrand.png'
+import CodeLabCommunity from "@/../public/images/Home/PrimaryProjects/CodeLabCommunity.png";
+import KommaMeetings from "@/../public/images/Home/PrimaryProjects/KommaMeetings.png";
+import HandsTogether from "@/../public/images/Home/PrimaryProjects/HandsTogether.png";
+import Translation from "@/../public/images/Home/PrimaryProjects/Translation.png";
+import CodeLabBrand from "@/../public/images/Home/PrimaryProjects/CodeLabBrand.png";
 // Secondary Projects
-import JAPA from '@/../public/images/Home/SecondaryProjects/JAPA.png'
-import Reddit from '@/../public/images/Home/SecondaryProjects/Reddit.png'
-import KommaInvolvement from '@/../public/images/Home/SecondaryProjects/KommaInvolvement.png'
+import Reddit from "@/../public/images/Home/SecondaryProjects/Reddit.png";
+import InterviewAce from "@/../public/images/Home/SecondaryProjects/InterviewAce.png";
+import JAPA from "@/../public/images/Home/SecondaryProjects/JAPA.png";
+import KommaInvolvement from "@/../public/images/Home/SecondaryProjects/KommaInvolvement.png";
 // Mini Projects
-import BobaTierList from '@/../public/images/Home/MiniProjects/BobaTierList.png'
-import DesignChallenge from '@/../public/images/Home/MiniProjects/DesignChallenge.png'
-import Geofilters from '@/../public/images/Home/MiniProjects/Geofilters.png'
-import AllergyLabels from '@/../public/images/Home/MiniProjects/AllergyLabels.png'
+import BobaTierList from "@/../public/images/Home/MiniProjects/BobaTierList.png";
+import DesignChallenge from "@/../public/images/Home/MiniProjects/DesignChallenge.png";
+import Geofilters from "@/../public/images/Home/MiniProjects/Geofilters.png";
+import AllergyLabels from "@/../public/images/Home/MiniProjects/AllergyLabels.png";
 
-import styles from '@/styles/index.module.css'
-import utils from '@/styles/utils.module.css'
+import styles from "@/styles/index.module.css";
+import utils from "@/styles/utils.module.css";
 
 export default function Home() {
   return (
@@ -33,10 +34,16 @@ export default function Home() {
       <Head>
         <title>Edward Chew / Coding and Designing</title>
 
-        <meta property="og:title" content="Edward Chew / Coding and Designing" />
+        <meta
+          property="og:title"
+          content="Edward Chew / Coding and Designing"
+        />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://edwardchew.vercel.app/" />
-        <meta property="og:image" content="https://edwardchew.vercel.app/images/Home/Edward.jpg" />
+        <meta
+          property="og:image"
+          content="https://edwardchew.vercel.app/images/Home/Edward.jpg"
+        />
       </Head>
 
       <section className={styles["intro"]}>
@@ -44,25 +51,34 @@ export default function Home() {
           <div className="row align-items-center">
             <div className={` ${styles["left"]} col-md-7 `}>
               <h1 className="bold">Hello there!</h1>
-              <h2>I’m Edward, a recent UC Davis grad based in the San Francisco Bay Area.</h2>
+              <h2>
+                I’m Edward, a recent UC Davis grad based in the San Francisco
+                Bay Area.
+              </h2>
               <p className="mobile">
-                Hey, I see you're on a mobile device. The mobile version of this website is still under construction. You can still see most of the site content, but some formatting may be off. Stay tuned!
+                Hey, I see you're on a mobile device. The mobile version of this
+                website is still under construction. You can still see most of
+                the site content, but some formatting may be off. Stay tuned!
               </p>
               <Link className="link" href="/about">
                 <span className={styles["link-text"]}>About Me</span>
-                <img className={`${styles["arrow-link"]} arrow-link`} src={ArrowWhite.src} alt="arrow"/>
+                <img
+                  className={`${styles["arrow-link"]} arrow-link`}
+                  src={ArrowWhite.src}
+                  alt="arrow"
+                />
               </Link>
             </div>
             <div className={`${styles["right"]} col-md-5`}>
-              <img src={Edward.src} alt="Photo of me!"/>
+              <img src={Edward.src} alt="Photo of me!" />
             </div>
           </div>
         </div>
       </section>
-      
+
       <section className={styles["work"]}>
         <h1>Here's some of my favorite work:</h1>
-        <PrimaryProject 
+        <PrimaryProject
           image={CodeLabCommunity}
           skills="LEADERSHIP"
           title="A New Community"
@@ -70,7 +86,7 @@ export default function Home() {
           tools=""
           link="/codelab"
         />
-        <PrimaryProject 
+        <PrimaryProject
           image={KommaMeetings}
           skills="FRONTEND, UI/UX DESIGN, & MORE"
           title="Fast and Friendly Meeting Scheduling"
@@ -78,7 +94,7 @@ export default function Home() {
           tools="React, Figma"
           link="/komma-meetings"
         />
-        <PrimaryProject 
+        <PrimaryProject
           image={HandsTogether}
           skills="FULL STACK DEVELOPMENT"
           title="Hands Together"
@@ -86,7 +102,7 @@ export default function Home() {
           tools="MongoDB, Express, React, Node.js, Amazon S3"
           link="/hands-together"
         />
-        <PrimaryProject 
+        <PrimaryProject
           image={Translation}
           skills="APPLIED ML/NLP"
           title="Evaluating Google Translate"
@@ -94,7 +110,7 @@ export default function Home() {
           tools="Python"
           link="/translation"
         />
-        <PrimaryProject 
+        <PrimaryProject
           image={CodeLabBrand}
           skills="FRONTEND, UI/UX & GRAPHIC DESIGN"
           title="CodeLab Brand Identity"
@@ -108,18 +124,25 @@ export default function Home() {
         <h1>Some other things I've worked on:</h1>
         <div className={` ${utils["gutter40-row"]} row `}>
           <SecondaryProject
-            image={JAPA}
-            skills="UI/UX DESIGN"
-            title="Reimagining Parking"
-            subtitle="Redesigning a smart parking mobile app for JAPA Inc."
-            link="/japa"
-          />
-          <SecondaryProject
             image={Reddit}
             skills="PYTHON WEB SCRAPING"
             title="Reddit Metadata Scraper"
             subtitle="Scraped Reddit subreddit metadata over several months using Python, PRAW, and Supervisord"
             link="/reddit"
+          />
+          <SecondaryProject
+            image={InterviewAce}
+            skills="FULL STACK & UI/UX DESIGN"
+            title="Ace Your Tech Interviews"
+            subtitle="Developed GPT-4-powered interview preparation tool with Node.js, Next.js, and Tailwind CSS"
+            link="/interview-ace"
+          />
+          <SecondaryProject
+            image={JAPA}
+            skills="UI/UX DESIGN"
+            title="Reimagining Parking"
+            subtitle="Redesigning a smart parking mobile app for JAPA Inc."
+            link="/japa"
           />
           <SecondaryProject
             image={KommaInvolvement}
@@ -164,36 +187,66 @@ export default function Home() {
 
       <section className={styles["experience"]}>
         <h1>My experience:</h1>
-        <p className={styles["position"]}><b className="demi">CodeLab</b> / Co-Founder, President</p>
+        <p className={styles["position"]}>
+          <b className="demi">CodeLab</b> / Co-Founder, President
+        </p>
         <p className={styles["years"]}>Oct 2020 - Jun 2023</p>
-        <p className={styles["position"]}><b className="demi">Computational Communication Lab at UC Davis</b> / Research Assistant</p>
+        <p className={styles["position"]}>
+          <b className="demi">Computational Communication Lab at UC Davis</b> /
+          Research Assistant
+        </p>
         <p className={styles["years"]}>Mar 2020 - Jan 2023</p>
-        <p className={styles["position"]}><b className="demi">IBM</b> / Software Engineering Intern</p>
+        <p className={styles["position"]}>
+          <b className="demi">IBM</b> / Software Engineering Intern
+        </p>
         <p className={styles["years"]}>Jun 2022 - Sep 2022</p>
-        <p className={styles["position"]}><b className="demi">UC Davis</b> / Student Web Developer</p>
+        <p className={styles["position"]}>
+          <b className="demi">UC Davis</b> / Student Web Developer
+        </p>
         <p className={styles["years"]}>May 2021 - Jun 2022</p>
-        <p className={styles["position"]}><b className="demi">Komma</b> / CPO, Software Engineer</p>
+        <p className={styles["position"]}>
+          <b className="demi">Komma</b> / CPO, Software Engineer
+        </p>
         <p className={styles["years"]}>Sep 2020 - Jun 2021</p>
-        <p className={styles["position"]}><b className="demi">Hands Together</b> / Software Engineer</p>
+        <p className={styles["position"]}>
+          <b className="demi">Hands Together</b> / Software Engineer
+        </p>
         <p className={styles["years"]}>Jan 2021 - May 2021</p>
-        <p className={styles["position"]}><b className="demi">Design Interactive</b> / Design Associate</p>
+        <p className={styles["position"]}>
+          <b className="demi">Design Interactive</b> / Design Associate
+        </p>
         <p className={styles["years"]}>Apr 2020 - May 2020</p>
-        <Link className="link" href="https://drive.google.com/file/d/1nTexryaBg258VKtiM36aMotkwGOm-t99/view?usp=sharing" target="_blank">
+        <Link
+          className="link"
+          href="https://drive.google.com/file/d/1nTexryaBg258VKtiM36aMotkwGOm-t99/view?usp=sharing"
+          target="_blank"
+        >
           <span className={styles["view-resume"]}>View Full Resume</span>
-          <img className="arrow-link" src={Arrow.src} alt="arrow"/>
+          <img className="arrow-link" src={Arrow.src} alt="arrow" />
         </Link>
       </section>
 
       <footer className={styles["footer"]}>
         <div className={styles["gradient-box"]}>
           <h1>Let's chat!</h1>
-          <p>Feel free to shoot me a message to get in touch!<br/><br/>
-            edwardchew8@gmail.com<br/>
-            <Link href="https://www.linkedin.com/in/edwardchew8/" target="_blank">linkedin.com/in/edwardchew8</Link>
+          <p>
+            Feel free to shoot me a message to get in touch!
+          </p>
+          <p>
+            <Link className="link" href='&#109;ailt&#111;&#58;%65dward%63%68ew8&#64;g%6&#68;ai%6&#67;&#46;%&#54;3%&#54;&#70;&#109;'>
+              <span>&#101;dwa&#114;dc&#104;ew8&#64;gma&#105;l&#46;c&#111;&#109;</span>
+            </Link>
+            <br />
+            <Link
+              className="link"
+              href="https://www.linkedin.com/in/edwardchew8/"
+              target="_blank"
+            >
+              <span>linkedin.com/in/edwardchew8</span>
+            </Link>
           </p>
         </div>
       </footer>
-
     </>
-  )
+  );
 }
---
+++ src/pages/interview-ace.tsx
@@ -0,0 +1,77 @@
+import type { ReactElement } from "react";
+import Link from "next/link";
+import Head from "next/head";
+
+import ProjectPageLayout from "../../components/layouts/ProjectPageLayout";
+
+import Logo from '@/../public/images/InterviewAce/Logo.svg';
+import Demo from '@/../public/images/InterviewAce/Demo.gif';
+
+import projStyles from "../styles/project-page.module.css";
+
+export default function InterviewAce() {
+  return (
+    <>
+      <Head>
+        <title>Interview Ace / Edward Chew</title>
+
+        <meta property="og:title" content="Interview Ace / Edward Chew" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://edwardchew.vercel.app/interview-ace" />
+        <meta property="og:image" content="https://edwardchew.vercel.app/images/Home/SecondaryProjects/InterviewAce.png" />
+      </Head>
+
+      <div className={` ${projStyles["project-page"]} `}>
+        <div className={projStyles["title"]}>
+          <img className={projStyles["logo"]} src={Logo.src} alt="Open AI logo"/>
+          <h1>Interview Ace</h1>
+          <h5 className="italic">Updated 9.4.2023</h5>
+        </div>
+
+        <section className="row">
+          <div className="col-md-9">
+            <h2 className="demi">The Job Hunt</h2>
+            <p className="small-body">
+              As my time at UC Davis wrapped up, I spent many hours applying to jobs and practicing problems on LeetCode. My confidence on technical interviews began increasing, but I hadn’t done nearly as much to prepare for behavioral rounds. Plus, in my interviews in the past, I was often asked high-level technical questions, which LeetCode wasn’t preparing me for.
+            </p>
+            <p className="small-body">
+              And so I built Interview Ace, a GPT-4-powered platform to prep for tech interviews. Users can access the large question bank, submit their responses to questions, and get AI feedback on their responses.
+            </p>
+            <p className="small-body">
+              The site isn't live, and it will likely stay that way since the OpenAI API is paid. You can view the GitHub repo <Link className="small-body" href="https://github.com/edward-chew/interview-ace" target="blank_">here</Link>.
+            </p>
+            <img src={Demo.src} alt="Project demo" className="no-shadow" />
+          </div>
+          <div className="col-md-3">
+            <h2 className="demi">Tech Stack</h2>
+            <p className="small-body">
+              <span className="demi">Backend</span>
+              <br/>Node.js
+              <br/>MongoDB
+              <br/>Mongoose
+              <br/>Express
+              <br/>Typescript
+              <br/>OpenAI API
+            </p>
+            <p className="small-body">
+              <span className="demi">Frontend</span>
+              <br/>Next.js
+              <br/>Tailwind CSS
+              <br/>shadcn/ui and Radix UI
+              <br/>SWR
+              <br/>Typescript
+            </p>
+          </div>
+        </section>
+      </div>
+    </>
+  );
+}
+
+InterviewAce.getLayout = (page : ReactElement) => {
+  return (
+    <ProjectPageLayout>
+      {page}
+    </ProjectPageLayout>
+  )
+}
